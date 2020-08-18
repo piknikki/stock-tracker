52bd10af0e467e0c1f76be3fc5a1d1f649df1510
Nikki Petersen <piknik@me.com>
2020-08-18 09:46:22 -0600

    got the api call working

---
+++ src/components/StockQuote.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="quote" v-if="stock.symbol">
+    <div class="row">
+      <div class="col-6">Symbol:</div>
+      <div class="col-6">{{stock.symbol}}</div>
+    </div>
+    <div class="row">
+      <div class="col-6">Company Name:</div>
+      <div class="col-6">{{stock.companyName}}</div>
+    </div>
+    <div class="row">
+      <div class="col-6">Latest Price (USD):</div>
+      <div class="col-6">{{stock.latestPrice}}</div>
+    </div>
+    <div class="row">
+      <div class="col-6">Change (USD):</div>
+      <div class="col-6">{{stock.change}}</div>
+    </div>
+    <div class="row">
+      <div class="col-6">Change (%):</div>
+      <div class="col-6">{{stock.changePercent}}</div>
+    </div>
+    <div class="row">
+      <div class="col-6">Latest Time:</div>
+      <div class="col-6">{{stock.latestTime}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['stock']
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,108 @@
 <template>
-  <div id="app">
-    <div id="header">
-      <h1>Header</h1>
-      <span class="green-items">${{ currentPrice }} / {{ percentage }}%</span>
+  <div class="container-fluid margins">
+    <div class="row">
+      <div class="col-10">
+        <input type="text"
+               autofocus placeholder="Enter symbol..." v-model="symbol" v-on:keyup.enter="search"/>
+      </div>
+      <div class="col-2">
+        <input type="button" value="Go" v-on:click="search" />
+      </div>
     </div>
 
-    <ul class="items" v-for="item in items" :key="item">
-      <li>
-        <span>{{ item.name }}</span>
-        <span class="price">{{ item.price }}</span>
-      </li>
-    </ul>
+    <stock-quote v-bind:stock="stock"></stock-quote>
+    <div class="alert alert-danger" role="alert" v-if="error">
+      <strong>Error!</strong> {{this.error}}
+    </div>
   </div>
 </template>
 
+<!--<template>-->
+<!--  <div id="app">-->
+<!--    <div id="header">-->
+<!--      <h1>Header</h1>-->
+<!--      <span class="green-items">${{ currentPrice }} / {{ percentage }}%</span>-->
+<!--    </div>-->
+
+<!--    <ul class="items" v-for="item in items" :key="item">-->
+<!--      <li>-->
+<!--        <span>{{ item.name }}</span>-->
+<!--        <span class="price">{{ item.price }}</span>-->
+<!--      </li>-->
+<!--    </ul>-->
+<!--    <div>-->
+<!--      <input type="text"-->
+<!--      placeholder="Enter symbol "-->
+<!--      v-model="symbol"-->
+<!--      >-->
+<!--      <button>Search</button>-->
+<!--    </div>-->
+<!--  </div>-->
+<!--</template>-->
+
 <script>
 // @ is an alias to /src
+import StockQuote from '@/components/StockQuote'
 
 export default {
-  name: 'Home',
   data () {
     return {
+      stock: {},
+      error: '',
+      symbol: ''
+    }
+    // return {
+    //   items: [
+    //     {
+    //       name: 'name 1',
+    //       price: 20.00,
+    //       currentPrice: 3.99,
+    //       percentage: 2.5
+    //     },
+    //     {
+    //       name: 'name 2',
+    //       price: 30.00,
+    //       currentPrice: 3.99,
+    //       percentage: 6.5
+    //     }
+    //   ]
+    // }
+  },
+  components: {
+    'stock-quote': StockQuote
+  },
+  methods: {
+    init () {
+      this.stock = {}
+      this.error = ''
+    },
+    search () {
+      const axios = require('axios')
 
-      items: [
-        {
-          name: 'name 1',
-          price: 20.00,
-          currentPrice: 3.99,
-          percentage: 2.5
-        },
-        {
-          name: 'name 2',
-          price: 30.00,
-          currentPrice: 3.99,
-          percentage: 6.5
+      axios({
+        method: 'GET',
+        url: 'https://investors-exchange-iex-trading.p.rapidapi.com/stock/' + this.symbol + '/book',
+        headers: {
+          'content-type': 'application/octet-stream',
+          'x-rapidapi-host': 'investors-exchange-iex-trading.p.rapidapi.com',
+          'x-rapidapi-key': '6c9880c44emsh99301f9d46cb5ebp11fbebjsn7fcfd3f93a9e',
+          useQueryString: true
         }
-      ]
+      })
+        .then((response) => {
+          this.stock = response.data.quote
+          console.log(response.data.quote)
+        })
+        .catch(this.handleErrors)
+    },
+    handleErrors (err) {
+      if (err.status === 404) {
+        this.error = ' That symbol could not be found. Please use another.'
+      } else {
+        this.stock = ''
+        this.error = ' There was an error retrieving the data. Please try again.'
+        console.log(err.response)
+      }
     }
   }
 }
