b6a7889d409e4639062a65dd6dae903d23bc89ac
Nikki Petersen <piknik@me.com>
2020-08-19 07:53:40 -0600

    add some branding

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,18 @@
   <div class="about">
     <h1>Welcome</h1>
     <p>Stock Grabber is an app that grabs a few bits and bobs of stock information for the stock symbol you input.</p>
-    <router-link to="/"><i class="fal fa-home fa-4x"></i></router-link>
+    <br>
+    <router-link to="/"><i class="fal fa-home fa-4x icons"></i></router-link>
+    <router-link to="/"><i class="fab fa-github-alt fa-4x icons"></i></router-link>
   </div>
 </template>
 
 <style>
 .fa-home {
   text-align: center;
+}
 
+a {
+  margin: 20px;
 }
 </style>
